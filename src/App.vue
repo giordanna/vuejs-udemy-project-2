b10651fe738c7b74869b6800d604b09d22b1828f
Giordanna De Gregoriis <gior.grs@gmail.com>
2019-01-03 12:01:11 -0300

    feat: Adição dos arquivos do projeto
    O desenvolvimento foi feito todo localmente primeiro, para depois inserir no github.

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="container">
+    	<app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+    	<app-quote-form @quoteAdded="addQuote($event)"></app-quote-form>
+    	<app-quotes-list :quotes="quotes" @quoteDeleted="deleteQuote($event)"></app-quotes-list>
+    	<app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+	import Header from './components/Header.vue'
+	import Footer from './components/Footer.vue'
+	import QuotesList from './components/QuotesList.vue'
+	import QuoteForm from './components/QuoteForm.vue'
+
+    export default {
+    	data: function() {
+    		return {
+    			quotes: [
+    				'Just a Quote to see something'
+    			],
+    			maxQuotes: 10
+    		}
+    	},
+        components: {
+        	'app-header': Header,
+        	'app-footer': Footer,
+        	'app-quotes-list': QuotesList,
+           	'app-quote-form': QuoteForm
+        },
+        methods: {
+        	addQuote(quote) {
+        		if (this.quotes.length >= this.maxQuotes) {
+        			alert('Please delete Quotes first!')
+        		}
+        		else {
+        			this.quotes.push(quote)
+        		}
+        	},
+        	deleteQuote(index) {
+        		this.quotes.splice(index, 1)
+        	}
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ src/components/QuotesList.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="row">
+		<app-quote-detail
+			v-for="(quote, index) in quotes"
+			@click.native="deleteQuote(index)"
+		>
+			{{ quote }}
+		</app-quote-detail>
+	</div>
+</template>
+
+<script>
+	import QuoteDetail from './QuoteDetail.vue'
+
+	export default {
+		props: ['quotes'],
+		components: {
+			'app-quote-detail': QuoteDetail
+		},
+		methods: {
+			deleteQuote(index) {
+				this.$emit('quoteDeleted', index)
+			}
+		}
+	}
+</script>
+
+<style></style>
